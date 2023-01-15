faf29d7c8831dbf4273eaa8c59dc2f9dab907c86
priyanka <priyankaraju92@gmail.com>
2023-01-15 18:55:51 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const button1 = document.getElementById('grid');
+button1.addEventListener('click', () => {
+    const newDiv = document.createElement('div');
+    console.log('Clicked');
+},false);
