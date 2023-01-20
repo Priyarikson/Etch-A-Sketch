2248da5c474e11cd2000e25277d8555fa2d81f74
priyanka <priyankaraju92@gmail.com>
2023-01-20 15:38:34 -0500

    need to work with footer buttons,everything else seems okay

--- index.js
+++ index.js
@@ -1,18 +1,78 @@
+
+
 document.addEventListener('DOMContentLoaded',()=>{
-    createDiv(16);
-    console.log('hi');
+    
+    let select0 = document.getElementById("pop-up");
+    select0.addEventListener("click",()=>{
+        let size2 = getSize();
+        console.log(size2);
+        createDiv(size2);
+    });
+
+    const button1 = document.getElementById('button1');
+    button1.addEventListener('click', ()=>{
+        console.log("clicked");
+    
+});
+    const button2 = document.getElementById('button2');
+    button2.addEventListener('click', ()=>{
+        console.log("clicked");
+    
+});
+    const button3 = document.getElementById('button3');
+    button3.addEventListener('click', ()=>{
+        console.log("clicked");
+});
+    
 });
 
+
+
 function createDiv(size){
-    let container = document.querySelector(".div-container");
-    container.style.gridTemplateColumns = `repeat(${size},1fr)`;
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    let container1 = document.querySelector(".container12");
+    container1.style.gridTemplateColumns = `repeat(${size},1fr)`;
+    container1.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    let numDivs = size*size;
+    let numDivs = size * size;
 
-    for(let i=0; i<numDivs; i++){
-        let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
-        container.insertAdjacentElement("beforeend", div);
+    for(let i = 0; i < numDivs; i++){
+        let div1 = document.createElement("div");
+        div1.addEventListener("mouseover",()=>{
+            div1.style.backgroundColor = "yellow";
+        });
+        container1.insertAdjacentElement("beforeend", div1);
+    };
+};
+
+
+function getSize(){
+    let input0 = prompt("Enter desired size?");
+    let message = document.querySelector(".message1");
+    if(input0 === ""){
+        message.innerHTML = "please provide a number";
     }
-}
+    else if(input0 < 0 || input0 > 100){
+        message.innerHTML = "provide a number between 1 and 100";
+    }
+    else{
+        message.innerHTML = "now you play";
+        return input0;
+    }
+};
+/* const randomColor = () => {
+	
+	const random = (min, max) => Math.random() * (max - min) + min
+
+	const h = Math.floor(random(0, 360))
+	const s = Math.floor(random(50, 100))
+	const l = Math.floor(random(50, 100))
+
+	return `hsl(${ h }, ${ s }%, ${ l }%)`
+  
+}*/
+
+
+    
+
+
+
